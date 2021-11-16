bc950537061fc221f20fdf30344eba1c7b79955e
Juan Vargas <juandeveloper19@gmail.com>
2021-11-16 06:41:52 -0500

    Se creo el componente que muestra cada uno de los gifs

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
+import GifGridItem from './GifGridItem';
+
 const GifGrid = ({ category }) => {
+	const [gifs, setGifs] = useState([]);
+
 	const getGifs = async () => {
 		const url = `https://api.giphy.com/v1/gifs/search?q=${category}&limit=12&api_key=n6GLf8ytDrbg22uMfGMIn73DjIcBNPlU`;
 
@@ -17,14 +21,23 @@ const GifGrid = ({ category }) => {
 		});
 
 		console.log(gifsData);
+		setGifs(gifsData);
 	};
 
-	getGifs();
+	useEffect(() => {
+		getGifs();
+	}, []);
 
 	return (
-		<div>
+		<React.Fragment>
 			<h3>{category}</h3>
-		</div>
+
+			<div className='card-grid'>
+				{gifs.map((img) => (
+					<GifGridItem key={img.id} {...img} />
+				))}
+			</div>
+		</React.Fragment>
 	);
 };
 
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-import './components/styles/global.css';
+import './styles.css';
 
 ReactDOM.render(
 	<React.StrictMode>
